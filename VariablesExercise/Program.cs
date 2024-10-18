aaded0a9ebca9ea8301e1063ca24281967dcd6ed
Riyard20 <kamarudeenriyad2@gmail.com>
2024-10-17 21:25:38 -0500

    declaration and intializing variables done

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿using System.Data.SqlTypes;
+// Declare and intialize variables
+
+string name = "John";
+int age= 31;
+char middleInitial = 'D';
+bool isOver18 = true; 
+double currentTemp = 77.9;
+decimal currentPrice = 29.99m;
+         
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"IsOver18: {isOver18}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
+
+
+    
+        
